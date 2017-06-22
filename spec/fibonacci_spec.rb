8a0d2d02553aeb1ce9dc50f906237be0bdb699a6
josephine adaoag <josephine.adaoag@gmail.com>
2017-06-22 15:02:37 -0400

    Added files fibonacci.rb and fibonacci_spec.rb

---
+++ spec/fibonacci_spec.rb
@@ -0,0 +1,11 @@
+require './fibonacci.rb'
+# Usage
+#`Fibonacci.find_number(0) # Returns 0`
+#`Fibonacci.find_number(6) # Returns 8`
+#`Fibonacci.find_number(10) # Returns 55`
+
+RSpec.describe Fibonacci do
+  it 'passes' do
+    expect(true).to be true
+  end
+end
