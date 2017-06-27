5f1a4f4f69d2c872f9794841ba8da5c23e1b2246
tonyestrada <tony.estrada@vitals.com>
2017-06-27 02:41:17 -0400

    Turn Fibonacci from a utility class into one that creates fibonacci objects

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -7,28 +7,37 @@ require './fibonacci.rb'
 
 RSpec.describe Fibonacci do
 
+  fibonacci = nil
+  let(:fib) { Fibonacci.new() }
+
   before(:example) do
+    fibonacci = fib
     puts "\n"
   end
 
-  # Fibonacci.find_number(0) returns 0
+  it "initializes an instance of fibonacci" do
+    fibonacci_instance = Fibonacci.new()
+    expect(fibonacci_instance).to be_an_instance_of(Fibonacci)
+  end
+
+  # fibonacci.find_number(0) returns 0
   it 'returns 0 when asked for the 0th fibonacci number' do
-    expect(Fibonacci.find_number(0)).to eq(0)
+    expect(fibonacci.find_number(0)).to eq(0)
   end
 
-  # Fibonacci.find_number(1) returns 1
+  # fibonacci.find_number(1) returns 1
   it 'returns 1 when asked for the 1st fibonacci number' do
-    expect(Fibonacci.find_number(1)).to eq(1)
+    expect(fibonacci.find_number(1)).to eq(1)
   end
 
-  # Fibonacci.find_number(6) returns 8
+  # fibonacci.find_number(6) returns 8
   it 'returns 8 when asked for the 6th fibonacci number' do
-    expect(Fibonacci.find_number(6)).to eq(8)
+    expect(fibonacci.find_number(6)).to eq(8)
   end
 
-  # Fibonacci.find_number(10) returns 55
+  # fibonacci.find_number(10) returns 55
   it 'returns 55 when asked for the 10th fibonacci number' do
-    expect(Fibonacci.find_number(10)).to eq(55)
+    expect(fibonacci.find_number(10)).to eq(55)
   end
 
 end
