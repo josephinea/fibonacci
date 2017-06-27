a63aed0600b6849da14f0cc4625216b32824294a
tonyestrada <tony.estrada@vitals.com>
2017-06-27 02:17:11 -0400

    Add specs and discover this should be done as an instance

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,2 +1,32 @@
 class Fibonacci
+
+  # by definition the first two numbers are either
+  # 0,1 or 1,1
+  # we went with 0,1
+  # start at 0
+  @@number = 0
+  @@first = 0
+  @@second = 1
+
+  # def self.find_number(ordinal)
+  #   return @@number if ordinal <= 0
+  #   ordinal -= 1
+  #   puts "num: #{@@number}, lst: #{@@last}, stl: #{@@second_to_last}"
+  #   @@number = @@last + @@second_to_last
+  #   @@second_to_last = @@last
+  #   @@last = @@number
+  #   self.find_number(ordinal)
+  # end
+
+  def self.find_number(number)
+    puts "number: #{number}, @@num: #{@@number}, @@first: #{@@first}, @@second: #{@@second}"
+    return @@first if number == 0
+    return @@second if number == 1
+    return @@number if number < 0
+    @@number = @@first + @@second
+    @@first = @@second
+    @@second = @@number
+    self.find_number(number - 1)
+  end
+
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,11 +1,34 @@
 require './fibonacci.rb'
-# Usage
-#`Fibonacci.find_number(0) # Returns 0`
-#`Fibonacci.find_number(6) # Returns 8`
-#`Fibonacci.find_number(10) # Returns 55`
+#  Usage
+# Fibonacci.find_number(0) returns 0
+# Fibonacci.find_number(1) returns 1
+# Fibonacci.find_number(6) returns 8
+# Fibonacci.find_number(10) returns 55
 
 RSpec.describe Fibonacci do
-  it 'passes' do
-    expect(true).to be true
+
+  before(:example) do
+    puts "\n"
   end
+
+  # Fibonacci.find_number(0) returns 0
+  it 'returns 0 when asked for the 0th fibonacci number' do
+    expect(Fibonacci.find_number(0)).to eq(0)
+  end
+
+  # Fibonacci.find_number(1) returns 1
+  it 'returns 1 when asked for the 1st fibonacci number' do
+    expect(Fibonacci.find_number(1)).to eq(1)
+  end
+
+  # Fibonacci.find_number(6) returns 8
+  it 'returns 8 when asked for the 6th fibonacci number' do
+    expect(Fibonacci.find_number(6)).to eq(8)
+  end
+
+  # Fibonacci.find_number(10) returns 55
+  it 'returns 55 when asked for the 10th fibonacci number' do
+    expect(Fibonacci.find_number(10)).to eq(55)
+  end
+
 end
