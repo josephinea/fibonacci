59d91545f906ee0ef7d2e5c8f98993311488ccb7
tonyestrada <tony.estrada@vitals.com>
2017-06-27 17:16:29 -0400

    Clean up specs to read better using let/subject/context

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,43 +1,42 @@
-require './fibonacci.rb'
-#  Usage
-# Fibonacci.find_number(0) returns 0
-# Fibonacci.find_number(1) returns 1
-# Fibonacci.find_number(6) returns 8
-# Fibonacci.find_number(10) returns 55
+require './fibonacci'
 
 RSpec.describe Fibonacci do
 
-  fibonacci = nil
-  let(:fib) { Fibonacci.new() }
+  context 'given we have an instance of Fibonacci' do
 
-  before(:example) do
-    fibonacci = fib
-    puts "\n"
-  end
+    subject { Fibonacci.new() }
+    let(:fibonacci_number) { subject.find_number(target) }
 
-  it "initializes an instance of fibonacci" do
-    fibonacci_instance = Fibonacci.new()
-    expect(fibonacci_instance).to be_an_instance_of(Fibonacci)
-  end
+    context 'when the target nuber is 0' do
 
-  # fibonacci.find_number(0) returns 0
-  it 'returns 0 when asked for the 0th fibonacci number' do
-    expect(fibonacci.find_number(0)).to eq(0)
-  end
+      let(:target) { 0 }
 
-  # fibonacci.find_number(1) returns 1
-  it 'returns 1 when asked for the 1st fibonacci number' do
-    expect(fibonacci.find_number(1)).to eq(1)
-  end
+      it 'finds the first number to be 0' do
+        expect(fibonacci_number).to eq(0)
+      end
 
-  # fibonacci.find_number(6) returns 8
-  it 'returns 8 when asked for the 6th fibonacci number' do
-    expect(fibonacci.find_number(6)).to eq(8)
-  end
+    end
 
-  # fibonacci.find_number(10) returns 55
-  it 'returns 55 when asked for the 10th fibonacci number' do
-    expect(fibonacci.find_number(10)).to eq(55)
+    context 'when the target number is 6' do
+
+      let(:target) { 6 }
+
+      it 'finds the sixth number to be 8' do
+        expect(fibonacci_number).to eq(8)
+      end
+
+    end
+
+    context 'when the target number is 10' do
+
+      let(:target) { 10 }
+
+      it 'finds the sixth number to be 55' do
+        expect(fibonacci_number).to eq(55)
+      end
+
+    end
+
   end
 
 end
